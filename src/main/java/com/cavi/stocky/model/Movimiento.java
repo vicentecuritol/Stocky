43f29be2d731d9cf2b7246a35b36dcb772ad7088
vicentecuritol <vi.curitol@duocuc.cl>
2026-04-22 11:27:15 -0400

    correccion del codigo con base de datos, añadiendo funciones al programa y eliminando codigo basura que no tiene ninguna funcion

--- src/main/java/com/cavi/stocky/model/Movimiento.java
+++ src/main/java/com/cavi/stocky/model/Movimiento.java
@@ -1,20 +1,14 @@
 package com.cavi.stocky.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 @Entity
 @Table(name = "movimiento")
 
@@ -23,13 +17,13 @@ public class Movimiento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    //private Long id;
+
     private String tipo; 
     private Integer cantidad;
     private LocalDateTime fecha;
     private String observacion;
     
-    
+    @ManyToOne
+    @JoinColumn (name = "producto_id")
     private Producto producto;
 }
--- src/main/java/com/cavi/stocky/model/Producto.java
+++ src/main/java/com/cavi/stocky/model/Producto.java
@@ -1,18 +1,17 @@
-package com.cavi.stocky.model;
+    package com.cavi.stocky.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
-@Data
+    @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 @Entity
 @Table(name = "producto")
 
@@ -22,12 +21,25 @@ public class Producto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     
     private Long id;
+
+    @NotBlank (message = "El nombre no puede estar vacio")
     private String nombre;
+
+    @NotNull (message = "El precio es obligatorio")
     private Integer precio;
+
+    @NotNull (message = "El stock actual es obligatorio")
+    @Min(value = 0, message = "El stock actual no puede ser negativo")
     private Integer stockActual;
+
+    @NotNull (message = "El stock minimo es obligatorio")
+    @Min(value = 0, message = "El stock minimo no puede ser negativo")
     private Integer stockMinimo;
     
-    // Relaciones simples como objetos
+    @ManyToOne
+    @JoinColumn (name = "categoria_id")
     private Categoria categoria;
+    @ManyToOne
+    @JoinColumn (name = "proveedor_id")
     private Proveedor proveedor;
 }
